75be2c22afca55ec604cb51f432890d89c83b7ec
arodriguez282005 <arodriguez282005@gmail.com>
2024-01-22 11:22:39 -0800

    add lines

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
  std::cout << "Luke Bowen" << std::endl;
  std::cout << "marshall Mullins" << std::endl;
  std::cout << "waleed niazi" << std::endl;
+ std::cout << "Alejandro Rodriguez" << std::endl;
  
 }
